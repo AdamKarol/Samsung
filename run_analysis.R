014919261eb2608834242797fee0a7ecaf4eee57
AdamKarol <akarolewski@o2.pl>
2015-12-27 23:42:42 +0100

    a
    a

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,5 @@
 #creating data set for analysis of Human Activity Recognition Using Smartphones
 setwd("D:/Adam Karolewski/Studia podyplomowe/Coursera/Data Science Specialization/Getting and Cleaning Data/Project")
-getwd()
 
 #1 load all data sets
 testX<-read.table("UCI HAR Dataset/test/X_test.txt")
@@ -14,27 +13,18 @@ trainsub<-read.csv("UCI HAR Dataset/train/subject_train.txt", header=FALSE)
 activity<-read.table("UCI HAR Dataset/activity_labels.txt")
 features<-read.table("UCI HAR Dataset/features.txt")
 
-head(trainX)
-names(trainX)
-head(trainX)
-
-tail(testX)
-names(testX)
-table(testX$V1)
-
-#binding together    
+#2 binding together    
 test<-cbind(testsub,testY,testX)
 train<-cbind(trainsub,trainY,trainX)
 all<-rbind(test,train)
 
-names(features)
+#3 activities names
 str_names<-c("person","activity",as.character(features$V2))
 colnames(all)<-str_names
 w<-grep(c("mean|std"), str_names)
 w<-c(1,2,w)
 all2<-all[,w]
 
-#3 activities names
 all2<-merge(all2,activity,by.x=c("activity"),by.y=c("V1"))
 
 #4 activities names
@@ -46,6 +36,4 @@ names(all2)
 groups<-group_by(all2, person,activity_name)
 avgs<-summarize_each(groups,funs(mean))
 avgs<-avgs[,-3]
-?summarize
-warnings()
 write.table(avgs, "means.txt",row.name=FALSE)
